d1b17d3366f095284ac414cc437964c6cdc3deae
Bluenix <bluenixdev@gmail.com>
2021-11-20 19:06:45 +0100

    Add Sticker resource data

---
+++ discord_typings/resources/sticker.py
@@ -0,0 +1,35 @@
+from typing import Optional, List
+
+from typing_extensions import Literal, NotRequired, TypedDict
+
+from ..shared import Snowflake
+from .user import UserData
+
+__all__ = ('StickerItemData', 'StickerData', 'StickerPackData')
+
+
+class StickerItemData(TypedDict):
+    id: Snowflake
+    name: str
+    format_type: Literal[1, 2, 3]
+
+
+class StickerData(StickerItemData):
+    pack_id: NotRequired[Snowflake]
+    description: Optional[str]
+    tags: str
+    type: Literal[1, 2]
+    available: NotRequired[bool]
+    guild_id: NotRequired[Snowflake]
+    user: NotRequired[UserData]
+    sort_value: NotRequired[int]
+
+
+class StickerPackData(TypedDict):
+    id: Snowflake
+    stickers: List[StickerData]
+    name: str
+    sku_id: Snowflake
+    cover_sticker_id: NotRequired[Snowflake]
+    description: str
+    banner_asset_id: Snowflake
