c7bb5f0ee5fabf4a2b27f99b52358aa019a69b1d
Bluenix <bluenixdev@gmail.com>
2021-11-20 19:25:03 +0100

    Commit Application and Teams structures

---
+++ discord_typings/resources/application.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import Optional, List
+
+from typing_extensions import TypedDict, NotRequired
+
+from ..shared import Snowflake
+from .user import UserData
+
+__all__ = ('ApplicationData', 'TeamData', 'TeamMemberData')
+
+
+class ApplicationData(TypedDict):
+    id: Snowflake
+    name: str
+    icon: Optional[str]
+    description: str
+    rpc_origins: List[str]
+    bot_public: bool
+    bot_require_code_grant: bool
+    terms_of_service_url: NotRequired[str]
+    privacy_policy_url: NotRequired[str]
+    owner: NotRequired[UserData]
+    summary: str
+    verify_key: str
+    team: Optional[TeamData]
+    guild_id: NotRequired[Snowflake]
+    primary_sku_id: NotRequired[Snowflake]
+    slug: NotRequired[str]
+    cover_image: NotRequired[str]
+    flags: NotRequired[int]
+
+
+class TeamData(TypedDict):
+    icon: Optional[str]
+    id: Snowflake
+    members: List[TeamMemberData]
+    name: str
+    owner_user_id: Snowflake
+
+
+class TeamMemberData(TypedDict):
+    membership_state: int
+    permissions: List[str]
+    team_id: Snowflake
+    user: UserData
