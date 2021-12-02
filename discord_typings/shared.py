afea6bab3699c58570d7de434acc4e2859efde0a
Bluenix <bluenixdev@gmail.com>
2021-12-02 21:25:45 +0100

    Create shared Snowflake alias
    This has weirdly not been committed yet but has been used in already committed code.

---
+++ discord_typings/shared.py
@@ -0,0 +1,4 @@
+# Somewhat useless but good to distinguish the difference, perhaps in the
+# future Python's typing system will have something similar to TypeScript's
+# "Template Literal Types" which is able to describe a snowflake.
+Snowflake = str
