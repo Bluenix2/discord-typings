f2e56eb6393bd48899c16f087229933ce3ead3df
Bluenix <bluenixdev@gmail.com>
2021-12-02 22:11:57 +0100

    Add Guild Template resource structures

---
+++ discord_typings/resources/guild_template.py
@@ -0,0 +1,20 @@
+from typing import Optional, TypedDict
+
+from ..shared import Snowflake
+
+from .user import UserData
+
+__all__ = ('GuildTemplateData',)
+
+class GuildTemplateData(TypedDict):
+    code: str
+    name: str
+    description: Optional[str]
+    usage_count: int
+    creator_id: Snowflake
+    creator: UserData
+    created_at: str
+    updated_at: str
+    source_guild_id: Snowflake
+    serialized_source_guild: GuildData
+    is_dirty: Optional[bool]
