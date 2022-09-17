7e74664f2b8d79f031577c621b4b88339049c381
Bluenix <bluenixdev@gmail.com>
2022-09-18 00:19:24 +0200

    Expand OAuth2 coverage by adding `AccessTokenResponseData`

--- discord_typings/oauth.py
+++ discord_typings/oauth.py
@@ -1,13 +1,26 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, final
 
 from typing_extensions import NotRequired, TypedDict
 
 from .resources.application import ApplicationData
 from .resources.user import UserData
 
-__all__ = ('AuthorizationInformation',)
+__all__ = ('AccessTokenResponseData', 'AuthorizationInformation')
+
+
+# https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-access-token-response
+
+
+@final
+class AccessTokenResponseData(TypedDict):
+    access_token: str
+    token_type: str
+    expires_in: int
+    refresh_token: str
+    scope: str
+
 
 # https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information
 
