45300c9b6211b01ce2c63f0d6ff466c9c2502b2c
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-10-10 20:40:42 +0000

    Add `ConnectionData` typing for user connections

--- discord_typings/resources/user.py
+++ discord_typings/resources/user.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 from typing_extensions import Literal, NotRequired, TypedDict, final
 
-__all__ = ('UserData', 'UserPremiumTypes')
+__all__ = ('UserData', 'UserPremiumTypes', 'ConnectionData', 'ConnectionTypes')
 
 if TYPE_CHECKING:
     from ..interactions import Locales
     from ..reference import Snowflake
+    from .guild import IntegrationData
+
 
 # https://discord.com/developers/docs/resources/user#user-object-user-structure
 
@@ -40,3 +42,41 @@ class UserData(UserBase):
 
 
 UserPremiumTypes = Literal[0, 1, 2]
+
+
+# https://discord.com/developers/docs/resources/user#connection-object
+
+
+ConnectionTypes = Literal[
+    'battlenet',
+    'ebay',
+    'epicgames',
+    'facebook',
+    'github',
+    'leagueoflegends',
+    'paypal',
+    'playstation',
+    'reddit',
+    'riotgames',
+    'spotify',
+    'skype',
+    'steam',
+    'twitch',
+    'twitter',
+    'xbox',
+    'youtube',
+]
+
+
+@final
+class ConnectionData(TypedDict):
+    id: str
+    name: str
+    type: ConnectionTypes
+    revoked: NotRequired[bool]
+    integrations: NotRequired[List[IntegrationData]]
+    verified: bool
+    friend_sync: bool
+    show_activity: bool
+    two_way_link: bool
+    visibility: Literal[0, 1]
