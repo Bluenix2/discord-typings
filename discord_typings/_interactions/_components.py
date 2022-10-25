2502bacdf8aec75e30c2edc62fd1087bbbfba060
Bluenix <bluenixdev@gmail.com>
2022-10-25 23:17:18 +0200

    Add typings for new select menu variants

--- discord_typings/_interactions/_components.py
+++ discord_typings/_interactions/_components.py
@@ -5,19 +5,21 @@ from typing import TYPE_CHECKING, List, Union
 from typing_extensions import Literal, NotRequired, TypedDict, final
 
 if TYPE_CHECKING:
-    from .._resources import EmojiData
+    from .._resources import ChannelTypes, EmojiData
 
 __all__ = (
     'ComponentTypes', 'ActionRowData', 'ButtonComponentData', 'ButtonStyles',
-    'SelectMenuComponentData', 'SelectMenuOptionData', 'TextInputComponentData',
-    'ComponentData'
+    'StringSelectMenuComponentData', 'UserSelectMenuComponentData',
+    'RoleSelectMenuComponentData', 'MentionableSelectMenuComponentData',
+    'ChannelSelectMenuComponentData', 'SelectMenuComponentData',
+    'SelectMenuOptionData', 'TextInputComponentData', 'ComponentData',
 )
 
 
 # https://discord.com/developers/docs/interactions/message-components#component-object-component-types
 
 
-ComponentTypes = Literal[1, 2, 3, 4]
+ComponentTypes = Literal[1, 2, 3, 4, 5, 6, 7, 8]
 
 
 # https://discord.com/developers/docs/interactions/message-components#action-rows
@@ -68,7 +70,7 @@ ButtonStyles = Literal[1, 2, 3, 4, 5]
 
 
 @final
-class SelectMenuComponentData(TypedDict):
+class StringSelectMenuComponentData(TypedDict):
     type: Literal[3]
     custom_id: str
     options: List[SelectMenuOptionData]
@@ -76,6 +78,54 @@ class SelectMenuComponentData(TypedDict):
     min_values: NotRequired[int]
     max_values: NotRequired[int]
     disabled: NotRequired[bool]
+
+
+@final
+class UserSelectMenuComponentData(TypedDict):
+    type: Literal[5]
+    custom_id: str
+    placeholder: NotRequired[str]
+    min_values: NotRequired[int]
+    max_values: NotRequired[int]
+    disabled: NotRequired[bool]
+
+
+@final
+class RoleSelectMenuComponentData(TypedDict):
+    type: Literal[6]
+    custom_id: str
+    placeholder: NotRequired[str]
+    min_values: NotRequired[int]
+    max_values: NotRequired[int]
+    disabled: NotRequired[bool]
+
+
+@final
+class MentionableSelectMenuComponentData(TypedDict):
+    type: Literal[7]
+    custom_id: str
+    placeholder: NotRequired[str]
+    min_values: NotRequired[int]
+    max_values: NotRequired[int]
+    disabled: NotRequired[bool]
+
+
+@final
+class ChannelSelectMenuComponentData(TypedDict):
+    type: Literal[8]
+    custom_id: str
+    channel_types: List[ChannelTypes]
+    placeholder: NotRequired[str]
+    min_values: NotRequired[int]
+    max_values: NotRequired[int]
+    disabled: NotRequired[bool]
+
+
+SelectMenuComponentData = Union[
+    StringSelectMenuComponentData, UserSelectMenuComponentData,
+    RoleSelectMenuComponentData, MentionableSelectMenuComponentData,
+    ChannelSelectMenuComponentData,
+]
 
 
 # https://discord.com/developers/docs/interactions/message-components#select-menu-object-select-option-structure
